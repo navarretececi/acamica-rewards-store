dd38bbf402b48a878d13c13aa5f65e6b16e2edb2
Cecilia Navarrete <navarretececi@gmail.com>
2021-07-30 23:49:07 -0300

    next prev pagination

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -4,15 +4,21 @@ import { AppContext } from "../../context/AppContext";
 
 export const Pagination =()=>{
     const { paginationList} = useContext(AppContext);
-        
+
     return (
         <div className="flex-row btn-arrow center">
-             <button onClick={()=>paginationList.prev()} className="container-arrow">
-                <img src="./images/arrow-left.svg" alt="flecha"/>
-            </button>
-            <button onClick={()=>paginationList.next()} className="container-arrow">
-                <img src="./images/arrow-right.svg" alt="flecha"/>
-            </button>
+            {
+                paginationList.currentPage > 1 ?
+                    <button onClick={()=>paginationList.prev()} className="container-arrow">
+                        <img src="./images/arrow-left.svg" alt="flecha"/>
+                    </button> : null
+            }
+            {
+               paginationList.currentPage !== paginationList.maxPage ?
+                    <button onClick={()=>paginationList.next()} className="container-arrow">
+                        <img src="./images/arrow-right.svg" alt="flecha"/>
+                    </button>: null
+            } 
         </div>
     )
 }
