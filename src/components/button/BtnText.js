f7a3167b19a600332c715cb1d9fbdf32dd5feea4
Cecilia Navarrete <navarretececi@gmail.com>
2021-08-02 21:02:48 -0300

    logo and title

--- src/components/button/BtnText.js
+++ src/components/button/BtnText.js
@@ -2,8 +2,8 @@ import "./BtnText.css";
 
 export const BtnText =(props)=>{
     return (
-        <button className="btn-filter flex-row center" onClick={props.handlerOnClick}>
-            <h5 className="normal-text center text-white">{props.text}</h5>
+        <button className={`btn-filter flex-row center ${props.classbutton}`} onClick={props.handlerOnClick}>
+            <h5 className={`normal-text center text-full ${props.classtext}`}>{props.text}</h5>
         </button>
     )
 }
