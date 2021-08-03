a2b631ff3217bbe2387544e6100dd58bd28f57a3
Cecilia Navarrete <navarretececi@gmail.com>
2021-08-03 12:49:18 -0300

    not found

---
+++ src/components/notification/NotFound.js
@@ -0,0 +1,9 @@
+import "./Notification.css";
+
+export const NotFound =()=>{
+        return (      
+            <div className="modal center">
+                <img src="./images/notFound.png" alt="error" />
+            </div>    
+        ) 
+}
