e06a08b60c4e3a5e633e60cf1183773401552ab6
Cecilia Navarrete <navarretececi@gmail.com>
2021-08-05 23:18:02 -0300

    test

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -8,7 +8,7 @@ export const Menu =()=>{
             <li className="pink-line"><Link className="normal-text" to="/">Home</Link></li>
             <li className="pink-line"><Link className="normal-text" to="/points">Add points</Link></li>
             <li className="pink-line"><Link className="normal-text" to="/history">History</Link></li>
-         </ul>
+        </ul>
    )
 }
 
